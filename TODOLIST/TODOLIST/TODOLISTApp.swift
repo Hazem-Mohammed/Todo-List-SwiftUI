4e1bbe1b14a1ef427db4badc175c9693740a9850
Hazem Mohammed <hazem.gouda@vodafone.com>
2022-11-15 12:28:36 +0200

    Initial commit

---
+++ TODOLIST/TODOLIST/TODOLISTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOLISTApp.swift
+//  TODOLIST
+//
+//  Created by Hazem Mohammed on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TODOLISTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
